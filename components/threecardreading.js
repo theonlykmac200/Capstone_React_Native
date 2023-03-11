ed4909065659e5bf77dc054648867997aefe4fb9
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2023-03-11 11:24:51 -0800

    heroku

--- components/threecardreading.js
+++ components/threecardreading.js
@@ -91,20 +91,26 @@ const cardImages = {
 
 export default function ThreeCardReading() {
   const [question, setQuestion] = useState('');
-  const [reading, setReading] = useState(3);
+  const [reading, setReading] = useState({});
 
   const fetchReading = async () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ question: question, rating: 3 }),
-    };
-    const response = await fetch('http://localhost:3002/threecard', requestOptions);
-    const data = await response.json();
-    setReading(data);
+    try {
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ question: question, rating: 3 }),
+      };
+      const response = await fetch('https://tarotnative.herokuapp.com/', requestOptions);
+      const data = await response.json();
+      setReading(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
+  
+
 
   return (
     <View>
