17aa5d6eeac2cb1c3db4569247ceec8e7ffda1e2
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2023-03-13 21:02:48 -0700

    spinner

--- components/ActivityMonitor.js
+++ components/ActivityMonitor.js
@@ -4,7 +4,7 @@ import { ActivityIndicator, View, Image } from 'react-native';
 const MyActivityIndicator = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Image
-      source={require('./images/Spinner.png')}
+      source={require('../images/Spinner.png')}
       style={{ width: 50, height: 50 }}
     />
   </View>
