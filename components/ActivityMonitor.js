32556d770902f2d0554ad0d8afa2a2e9fe543d97
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2023-03-13 21:06:34 -0700

    spinner

--- components/ActivityMonitor.js
+++ components/ActivityMonitor.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { ActivityIndicator, View, Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { ActivityIndicator, View, Image, Animated } from 'react-native';
 
-const MyActivityIndicator = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Image
-      source={require('../images/Spinner.png')}
-      style={{ width: 50, height: 50 }}
-    />
-  </View>
-);
+const MyActivityIndicator = () => {
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [spinValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Animated.Image
+        source={require('../images/Spinner.png')}
+        style={{ width: 125, height: 150, transform: [{ rotate: spin }] }}
+      />
+    </View>
+  );
+};
 
 export default MyActivityIndicator;
